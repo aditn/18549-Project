5fa76d7b74645d1f9872ba1ce413b79b25137086
Adit Namdev <aditn4@gmail.com>
2017-04-09 20:18:23 -0400

    changed format of data being written to serial port

--- Controller/sensor.c
+++ Controller/sensor.c
@@ -3,6 +3,7 @@
 */
 
 #include "sensor.h"
+#include <avr/interrupt.h>
 //#include "I2C-master-lib-master/i2c_master.h"
 
 uint16_t adc_read(uint8_t adcx) {
@@ -31,15 +32,59 @@ uint16_t adc_read(uint8_t adcx) {
   return adcV;
 }
 
+void adc_init(){
+   // disable interrupts
+   cli();
 
-void collectforceData(uint16_t* data){
-  // use ADC here later
+   // Set the ADC prescaler to 128 (i.e., 16MHz/128 = 125KHz)
+   ADCSRA |= ( 1 << ADPS2 ) | ( 1 << ADPS1 ) | ( 1 << ADPS0 );
 
+   // Set the voltage reference from AVcc (i.e., 5V).
+   ADMUX |= ( 1 << REFS0 );
+
+   // Turn on the ADC.
+   ADCSRA |= ( 1 << ADEN );
+
+   // Do the initial conversion (i.e., the slowest conversion)
+   // to ensure that everything is up and running.
+   ADCSRA |= ( 1 << ADSC );
+   
+   //enable interrupts
+   sei();
+}
+
+uint32_t ReadCount(void){
+  uint32_t Count;
+  uint8_t i;
+  //PC0 is clk
+  PORTC &= ~(1<<PC0);
+  //PC1 is data
+  PORTC |= (1<<PC1); // set PC1 as input
+  Count=0;
+  while(PINC & (1<<PC1));
+  for (i=0;i<24;i++){
+    PORTC |= (1<<PC0);
+    Count=Count<<1;
+    PORTC &= ~(1<<PC0);
+    if(PINC & (1<<PC1)) Count++;
+  }
+  
+  PORTC |= (1<<PC0);
+  Count=Count^0x800000;
+  PORTC &= ~(1<<PC0);
+  return(Count);
+} 
+void collectforceData(uint32_t* data){
+  // set Data and Manual CLK pins
+  DDRC |= (1<<PC0); // Set PC0(clk) as an output pin
+  DDRC &= ~(1<<PC1); // Set PC1(data) as input pin
+  
   // using fake data for now
-  data[0] = (uint16_t)10;
-  data[1] = (uint16_t)20;
-  data[2] = (uint16_t)30;
-  data[3] = (uint16_t)40;
+  data[0] = ReadCount();
+  //data[0] = (uint32_t)10;
+  data[1] = (uint32_t)20;
+  data[2] = (uint32_t)30;
+  data[3] = (uint32_t)40;
 }
 
 void read_Accelerometer(float* accelArray){
